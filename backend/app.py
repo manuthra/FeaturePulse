412c4f1a157f788bcc745c6424b6717315a50655
unknown <manuthramohan@gmail.com>
2026-05-22 20:37:43 +0530

    fixed render deployment

--- backend/app.py
+++ backend/app.py
@@ -136,5 +136,6 @@ def create_jira():
 
 # ---------------- RUN APP ---------------- #
 import os
-
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
